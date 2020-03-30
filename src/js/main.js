6abf7aad5b8aa0c9386d1f05c10ed19d0e686f8e
AleksejBarzykin <aleksejbarzykin@gmail.com>
2020-03-30 03:32:43 +0300

    Добавил главный блок

--- src/js/main.js
+++ src/js/main.js
@@ -1,80 +1,18 @@
-// console.log(multiple(4, 2));
-// console.log(multiple(6, 2));
-// console.log(multiple(8, 2));
+let btnText = document.querySelectorAll(".btn-list__title");
 
+/* Смена цвета кнопки по клику*/
 
-
-// function btnBgColor() {
-// 	this.style.background = 'green';
-// }
-
-// function green() {
-// 	this.style.background = 'green'
-// };
-// function red() {
-// 	this.style.background = 'red'
-// };
-
-
-
-
-// containerBtn = document.querySelectorAll('.container-title');
-// containerBtn.forEach(function (element) {
-// 	element.onclick = green;
-// })
-
-
-
-let big = document.querySelector(".bigg"),
-	flag = "",
-	colorBtn = ['green'];
-
-function change() {
-
-	for (let i = 0; i < colorBtn.length; i++) {
-
-		if (flag == "") {
-			big.className = 'bigg green';
-			flag = "green";
-		}
-		else {
-			big.className = "bigg";
-			flag = "";
-		}
-	}
+for (let i = 0; i < btnText.length; i++) {
+	btnText[i].onclick = () => {
+		btnText[i].classList.toggle("colorClick");
+	};
 }
-big.onclick = change;
+// console.log(btnText[i]);
 
 
 
 
 
-
-// let big = document.querySelectorAll(".container-title"),
-// 	flag = "",
-// 	colorBtn = ['green'];
-
-// function change() {
-
-// 	for (let i = 0; i < colorBtn.length; i++) {
-
-// 		if (flag == "") {
-// 			big.className = 'container-title green';
-// 			flag = "green";
-// 		}
-// 		else {
-// 			big.className = "container-title";
-// 			flag = "";
-// 		}
-// 	}
-// }
-// big.onclick = change;
-
-
-// let elementClasses = elem.classList;
-// const button = document.querySelector('button');
-// let elem = document.querySelector("#bigg");
-// elem.onclick = console.log('456')
 
 
 
